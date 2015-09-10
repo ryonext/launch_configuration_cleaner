699fe42e7a368c262a0481d4952ecaa5c5bbfde6
ryonext <ryonext.s@gmail.com>
2015-09-10 19:07:51 +0900

    Refactoring methods.

--- cleaner.rb
+++ cleaner.rb
@@ -9,32 +9,39 @@ def launch_configurations
   @launch_configurations ||= client.describe_launch_configurations.launch_configurations
 end
 
-puts "These are your launch configurations"
-puts "["
-launch_configurations.each do |l|
-  puts "  #{l.launch_configuration_name},"
-end
-puts "]"
-
-delete_list = []
-
-launch_configurations.each do |l|
-  puts "Do you want to delete #{l.launch_configuration_name}? (y/n)"
-  answer = gets.strip
-  if answer == "y"
-    delete_list << l
-    puts "Delete #{l.launch_configuration_name} later..."
+def list_current_configurations
+  puts "These are your launch configurations"
+  puts "["
+  launch_configurations.each do |l|
+    puts "  #{l.launch_configuration_name},"
   end
+  puts "]"
 end
 
+def ask_delete_target
+  launch_configurations.map do |l|
+    puts "Do you want to delete #{l.launch_configuration_name}? (y/n)"
+    answer = gets.strip
+    if answer == "y"
+      puts "Delete #{l.launch_configuration_name} later..."
+      l
+    end
+  end.compact
+end
+
+def list_delete_target(delete_list)
+  puts "These launch configurations will be deleted."
+  puts "["
+  delete_list.each do |l|
+    puts "  #{l.launch_configuration_name},"
+  end
+  puts "]"
+end
+
+delete_list = ask_delete_target
 exit if delete_list.size == 0
 
-puts "These launch configurations will be deleted."
-puts "["
-delete_list.each do |l|
-  puts "  #{l.launch_configuration_name},"
-end
-puts "]"
+list_delete_target(delete_list)
 
 while true do
   puts "Are you sure? (yes or no)"
@@ -43,8 +50,10 @@ while true do
     delete_list.each do |l|
       client.delete_launch_configuration(launch_configuration_name: l.launch_configuration_name)
     end
+    puts "LaunchConfiguration have been deleted."
     break
   elsif final_answer == "no"
+    puts "Ok, canceled."
     break
   else
     puts "Sorry, I don't understand your answer."
