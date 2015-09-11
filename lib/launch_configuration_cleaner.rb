d8bd534deb716300c26b4148e12b8f57c5e72349
ryonext <ryonext.s@gmail.com>
2015-09-11 22:00:00 +0900

    Add files for gems.

---
+++ lib/launch_configuration_cleaner.rb
@@ -0,0 +1,5 @@
+require "launch_configuration_cleaner/version"
+
+module LaunchConfigurationCleaner
+  # Your code goes here...
+end
---
+++ lib/launch_configuration_cleaner/version.rb
@@ -0,0 +1,3 @@
+module LaunchConfigurationCleaner
+  VERSION = "0.1.0"
+end
